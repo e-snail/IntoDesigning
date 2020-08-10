44b059c6a3956e76def2dbb0d9a9b87987a85cee
WilburWu <wilbur.k.woo@gmail.com>
2020-08-10 21:03:20 +0800

    初始Android项目

---
+++ app/src/main/java/into/designing/MainActivity.java
@@ -0,0 +1,20 @@
+package into.designing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author wilbur.wu
+ * @date 2020.08.10
+ *
+ * 主 Activity 入口
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
